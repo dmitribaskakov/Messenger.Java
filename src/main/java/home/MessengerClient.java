7c461f10c9fbd7052be8cb06368b6325871b05aa
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-29 18:44:04 +0700

    Initial Maven project

---
+++ src/main/java/home/MessengerClient.java
@@ -0,0 +1,9 @@
+package home;
+
+public class MessengerClient {
+
+    public static void main(String... args) { //throws Exception
+        System.out.println("Initialize Messenger Client");
+    }
+
+}
---
+++ src/main/java/home/MessengerServer.java
@@ -0,0 +1,10 @@
+package home;
+
+public class MessengerServer {
+
+    public static void main(String... args) { //throws Exception
+        System.out.println("Initialize Messenger Server");
+    }
+
+}
+
