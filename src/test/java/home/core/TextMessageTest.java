bce661b494c2978970ee37504b62e377e0d370e2
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-29 22:02:08 +0700

    Add class TextMessageTest

---
+++ src/test/java/home/core/TextMessageTest.java
@@ -0,0 +1,51 @@
+package home.core;
+
+//import java.util.HashMap;
+//import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TextMessageTest {
+
+//    static final Map<String, String> testData = new HashMap<>();
+
+    @org.junit.Before
+    public void setUp() {
+//        testData.put("123", "123");
+    }
+
+    @org.junit.Test
+    public void testEquals() {
+        TextMessage actual = new TextMessage();
+        actual.setType(TypeOfMessage.MSG_TEXT);
+        actual.setSenderId(0L);
+        actual.setText("Text");
+
+        TextMessage expected = new TextMessage();
+        expected.setType(TypeOfMessage.MSG_TEXT);
+        expected.setSenderId(0L);
+        expected.setText("Text");
+        assertEquals(actual, expected);
+
+        expected.setType(TypeOfMessage.MSG_INFO);
+        expected.setSenderId(0L);
+        expected.setText("Text");
+        assertNotEquals(actual, expected);
+
+        expected.setType(TypeOfMessage.MSG_TEXT);
+        expected.setSenderId(1L);
+        expected.setText("Text");
+        assertNotEquals(actual, expected);
+
+        expected.setType(TypeOfMessage.MSG_TEXT);
+        expected.setSenderId(0L);
+        expected.setText("tEXT");
+        assertNotEquals(actual, expected);
+
+        String st = "String";
+        assertNotEquals(actual, st);
+
+        expected = null;
+        assertNotEquals(actual, expected);
+    }
+}
