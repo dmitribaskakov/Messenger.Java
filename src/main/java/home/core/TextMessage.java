eb7cad0d8dbd40b8b07cc649d940a53f585e29c2
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-29 20:52:17 +0700

    Add class TextMessage

---
+++ src/main/java/home/core/TextMessage.java
@@ -0,0 +1,37 @@
+package home.core;
+
+import java.util.Objects;
+
+/**
+ * Текстовое сообщение
+ */
+public class TextMessage extends Message {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!super.equals(other)) return false;
+        TextMessage message = (TextMessage) other;
+        return Objects.equals(text, message.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), text);
+    }
+
+    @Override
+    public String toString() {
+        return "TextMessage{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
