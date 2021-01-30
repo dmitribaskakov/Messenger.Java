97005fa1adf7b45122de58a2cd7ed24719eb2a14
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-30 07:12:25 +0700

    Realize simple clientSocket

--- src/main/java/home/MessengerServer.java
+++ src/main/java/home/MessengerServer.java
@@ -1,5 +1,12 @@
 package home;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 //import java.util.concurrent.PriorityBlockingQueue;
 
 public class MessengerServer {
