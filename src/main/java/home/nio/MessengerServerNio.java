1343038b6d78de5216033ca2742b59e3fcafe2de
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-02-01 14:37:21 +0700

    Add MessengerServerNio.run

--- src/main/java/home/nio/MessengerServerNio.java
+++ src/main/java/home/nio/MessengerServerNio.java
@@ -3,14 +3,13 @@ package home.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import static home.nio.ChangeRequest.CHANGEOPS;
 
 import static java.nio.ByteBuffer.allocate;
 import static java.nio.channels.SelectionKey.OP_ACCEPT;
@@ -21,17 +20,53 @@ public class MessengerServerNio {
 
     private Selector selector;
     private ByteBuffer readBuffer = allocate(8192);
-    private WorkerResponseToMessage workerResponseToMessage = WorkerResponseToMessage EchoWorker();
+    private WorkerResponseToMessage workerResponseToMessage = new WorkerResponseToMessage();
     private final List<ChangeRequest> changeRequests = new LinkedList<>();
     private final Map<SocketChannel, List<ByteBuffer>> pendingData = new HashMap<>();
 
     private MessengerServerNio() throws IOException {
         ServerSocketChannel serverChannel = ServerSocketChannel.open();
         serverChannel.configureBlocking(false);
-        serverChannel.socket().bind(new InetSocketAddress(ADDRESS, PORT););
+        serverChannel.socket().bind(new InetSocketAddress(ADDRESS, PORT));
         selector = SelectorProvider.provider().openSelector();
         serverChannel.register(selector, OP_ACCEPT);
         new Thread(workerResponseToMessage).start();
+    }
+    public static void main(String[] args) throws IOException {
+        new MessengerServerNio().run();
+    }
+
+    private void run() throws IOException {
+        while (true) {
+            synchronized (changeRequests) {
+                for (ChangeRequest change : changeRequests) {
+                    switch (change.type) {
+                        case CHANGEOPS:
+                            SelectionKey key = change.socket.keyFor(selector);
+                            key.interestOps(change.ops);
+                            break;
+                        default:
+                    }
+                }
+                changeRequests.clear();
+            }
+            selector.select();
+            Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
+            while (selectedKeys.hasNext()) {
+                SelectionKey key = selectedKeys.next();
+                selectedKeys.remove();
+                if (!key.isValid()) {
+                    continue;
+                }
+                if (key.isAcceptable()) {
+                    accept(key);
+                } else if (key.isReadable()) {
+                    read(key);
+                } else if (key.isWritable()) {
+                    write(key);
+                }
+            }
+        }
     }
 
 }
